7c4520aebba170e704dcaca5e273bed7d204536c
ThePillowCat <noahlevy575@gmail.com>
2022-10-28 19:31:07 -0400

    Update game.py
    Template of level

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 from pygame import *
 
-width,height=800,600
-screen=display.set_mode((width,height))
+width,height=800,800
+screen=display.set_mode((1000, 800))
 RED=(255,0,0)
 GREY=(127,127,127)
 BLACK=(0,0,0)
@@ -11,15 +11,56 @@ YELLOW=(255,255,0)
 
 running=True
 
+myClock = time.Clock()
+
+levels = [[[(200, 300, 100, 50), (500, 400, 100, 50)]]]
+
+class Entity:
+    def __init__(self, x, y):
+        self.posX = x
+        self.posY = y
+        self.jumping = False
+        self.vy = -8
+
+    def movePlayer(self):
+        keys = key.get_pressed()
+        if keys[K_a]:
+            self.posX -= 5
+        if keys[K_d]:
+            self.posX += 5
+        if self.jumping:
+            self.vy += 0.25
+            self.posY += self.vy
+            if self.posY > 780:
+                self.jumping = False
+                self.vy = -8
+                self.posY = 780
+
+class Level():
+    def __init__(self):
+        pass
+
+player = Entity(20, 780)
+
 while running:
+    screen.fill(0)
+
+    mx,my=mouse.get_pos()
+    mb=mouse.get_pressed()
+
     for evt in event.get():
         if evt.type==QUIT:
             running=False
-                       
-    mx,my=mouse.get_pos()
-    mb=mouse.get_pressed()
-      
-   
+        if evt.type==KEYDOWN:
+            if evt.key == K_SPACE and not player.jumping:
+                player.jumping = True
+
+    player.movePlayer()
+
+    draw.circle(screen, GREEN, (player.posX, player.posY), 20)
+    for i in levels[0][0]:
+        draw.rect(screen,BLUE,i)
+    myClock.tick(60)
     display.flip()
             
 quit()
