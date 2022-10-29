790b93b786b086a469365956a24d456751d4114a
ThePillowCat <noahlevy575@gmail.com>
2022-10-28 23:46:26 -0400

    Update game.py
    prototype 2
    Still need to fix gravity bug

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from pygame import *
+from math import *
 
 width,height=1000,800
 screen=display.set_mode((width, height))
@@ -13,49 +14,58 @@ running=True
 
 myClock = time.Clock()
 
-levels = [[[(200, 300, 100, 50), (500, 400, 100, 50), (100, 765, 100, 50)]]]
+levels = [[[],[],[]]]
 
 class Entity:
     def __init__(self, x, y):
         self.posX = x
         self.posY = y
-        self.jumping = False
         self.vy = -8
+        self.jumping = False
+        self.grounded = False
+        self.falling = False
 
     def movePlayer(self):
 
         keys = key.get_pressed()
 
         if keys[K_a]:
-                self.posX -= 5
-        for platform in levels[level.currentLevel][level.getSquare(player.posY)]:
-            r1 = Rect(self.posX-20, self.posY-20, 20, 20)
-            r2 = Rect(platform)
-            if Rect.colliderect(r1, r2):
-                self.posX += 5
-
-        if keys[K_d]:
-                self.posX += 5
-        for platform in levels[level.currentLevel][level.getSquare(player.posY)]:
-            r1 = Rect(self.posX-20, self.posY-20, 20, 20)
-            r2 = Rect(platform)
-            if Rect.colliderect(r1, r2):
-                self.posX -= 5
-
-        #add gravity
-        if self.jumping:
-            self.vy += 0.25
-            self.posY += self.vy
+            self.posX -= 5
             for platform in levels[level.currentLevel][level.getSquare(player.posY)]:
                 r1 = Rect(self.posX-20, self.posY-20, 40, 40)
                 r2 = Rect(platform)
-                if Rect.colliderect(r1, r2) or self.posY > 780:
-                    self.posY-= self.vy
-                    self.posY -= 0.25
-                    self.jumping = False
-                    self.vy = -8
-                    self.useGravity = True
-        
+                if Rect.colliderect(r1, r2) or self.posX-20 < 0:
+                    self.posX += 5
+
+        if keys[K_d]:
+            self.posX += 5
+            for platform in levels[level.currentLevel][level.getSquare(player.posY)]:
+                r1 = Rect(self.posX-20, self.posY-20, 40, 40)
+                r2 = Rect(platform)
+                if Rect.colliderect(r1, r2) or self.posX+20 > width:
+                    self.posX -= 5
+
+        if self.jumping:
+            self.vy += 0.25
+            self.posY += self.vy
+            if self.posY > 780 and level.getSquare(self.posY) == 0:
+                self.posY = 780
+                self.jumping = False
+                self.grounded = True
+                self.vy = -8
+            else: 
+                for platform in levels[level.currentLevel][level.getSquare(player.posY)]:
+                    r1 = Rect(self.posX-20, self.posY-20, 40, 40)
+                    r2 = Rect(platform)
+                    if Rect.colliderect(r1, r2):
+                        if r2[1] < r1[1]+40 < r2[1]+r2[3]:
+                            self.posY = r2[1]-20
+                            self.vy = -8
+                            self.jumping = False
+                        else:
+                            self.vy = 1
+                        break
+
         draw.circle(screen, GREEN, (player.posX, player.posY), 20)
 
 class Level():
@@ -64,19 +74,25 @@ class Level():
         self.square = 0
 
     def getSquare(self, y):
-        if y < 0:
-            self.square +=1 
+        if y < 0 and self.square < 2:
+            self.square+=1
+            player.posY = height-30
+            print("This ran 1")
+        elif y > height and self.square > 0:
+            self.square-=1
+            player.posY = 30
+            print("This ran 2")
         return(self.square)
 
     def draw(self):
         for platform in levels[self.currentLevel][self.getSquare(player.posY)]:
-            draw.rect(screen, BLUE, platform)
+            draw.rect(screen, RED, platform)
 
 player = Entity(20, 780)
 level = Level()
 
 while running:
-    screen.fill(0)
+    screen.fill((255, 255, 255))
 
     mx,my=mouse.get_pos()
     mb=mouse.get_pressed()
@@ -87,6 +103,9 @@ while running:
         if evt.type==KEYDOWN:
             if evt.key == K_SPACE and not player.jumping:
                 player.jumping = True
+        if evt.type == MOUSEBUTTONDOWN:
+            if evt.button == 1:
+                levels[0][level.getSquare(player.posY)].append((mx, my, 100, 50))
 
     player.movePlayer()
     level.draw()
