5d1bebd96e58071710816b1b12ccfb0ef089bd9a
ThePillowCat <noahlevy575@gmail.com>
2022-10-28 20:34:19 -0400

    Update game.py
    Prototype 1

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 from pygame import *
 
-width,height=800,800
-screen=display.set_mode((1000, 800))
+width,height=1000,800
+screen=display.set_mode((width, height))
 RED=(255,0,0)
 GREY=(127,127,127)
 BLACK=(0,0,0)
@@ -13,7 +13,7 @@ running=True
 
 myClock = time.Clock()
 
-levels = [[[(200, 300, 100, 50), (500, 400, 100, 50)]]]
+levels = [[[(200, 300, 100, 50), (500, 400, 100, 50), (100, 765, 100, 50)]]]
 
 class Entity:
     def __init__(self, x, y):
@@ -23,24 +23,57 @@ class Entity:
         self.vy = -8
 
     def movePlayer(self):
+
         keys = key.get_pressed()
+
         if keys[K_a]:
-            self.posX -= 5
+                self.posX -= 5
+        for platform in levels[level.currentLevel][level.getSquare(player.posY)]:
+            r1 = Rect(self.posX-20, self.posY-20, 20, 20)
+            r2 = Rect(platform)
+            if Rect.colliderect(r1, r2):
+                self.posX += 5
+
         if keys[K_d]:
-            self.posX += 5
+                self.posX += 5
+        for platform in levels[level.currentLevel][level.getSquare(player.posY)]:
+            r1 = Rect(self.posX-20, self.posY-20, 20, 20)
+            r2 = Rect(platform)
+            if Rect.colliderect(r1, r2):
+                self.posX -= 5
+
+        #add gravity
         if self.jumping:
             self.vy += 0.25
             self.posY += self.vy
-            if self.posY > 780:
-                self.jumping = False
-                self.vy = -8
-                self.posY = 780
+            for platform in levels[level.currentLevel][level.getSquare(player.posY)]:
+                r1 = Rect(self.posX-20, self.posY-20, 40, 40)
+                r2 = Rect(platform)
+                if Rect.colliderect(r1, r2) or self.posY > 780:
+                    self.posY-= self.vy
+                    self.posY -= 0.25
+                    self.jumping = False
+                    self.vy = -8
+                    self.useGravity = True
+        
+        draw.circle(screen, GREEN, (player.posX, player.posY), 20)
 
 class Level():
     def __init__(self):
-        pass
+        self.currentLevel = 0
+        self.square = 0
+
+    def getSquare(self, y):
+        if y < 0:
+            self.square +=1 
+        return(self.square)
+
+    def draw(self):
+        for platform in levels[self.currentLevel][self.getSquare(player.posY)]:
+            draw.rect(screen, BLUE, platform)
 
 player = Entity(20, 780)
+level = Level()
 
 while running:
     screen.fill(0)
@@ -56,10 +89,8 @@ while running:
                 player.jumping = True
 
     player.movePlayer()
+    level.draw()
 
-    draw.circle(screen, GREEN, (player.posX, player.posY), 20)
-    for i in levels[0][0]:
-        draw.rect(screen,BLUE,i)
     myClock.tick(60)
     display.flip()
             
