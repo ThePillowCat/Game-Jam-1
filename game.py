37ec057d2328ab70fb8ea2e60d8934e8491173d4
ThePillowCat <noahlevy575@gmail.com>
2022-10-29 12:32:38 -0400

    Update game.py
    Prototype with working gravity lol

--- game.py
+++ game.py
@@ -21,13 +21,37 @@ class Entity:
         self.posX = x
         self.posY = y
         self.vy = -8
+        self.gravityVy = 1
         self.jumping = False
-        self.grounded = False
         self.falling = False
+        self.grounded = True
 
     def movePlayer(self):
 
         keys = key.get_pressed()
+
+        self.grounded = False
+
+        if not self.grounded and not self.jumping:
+            self.falling = True
+            self.gravityVy += 0.125
+            self.posY += self.gravityVy
+
+        if (level.getSquare(player.posY) == 0 and self.posY >= 780):
+            self.grounded = True
+            self.posY = 780
+            self.gravityVy = 1
+            self.falling = False
+
+        for platform in levels[level.currentLevel][level.getSquare(player.posY)]:
+            r1 = Rect(self.posX-20, self.posY-20, 40, 42)
+            r2 = Rect(platform)
+            if Rect.colliderect(r1, r2):
+                if (r2[1] < r1[1]+42 < r2[1]+r2[3]):
+                    self.posY = r2[1]-20
+                    self.grounded = True
+                    self.falling = False
+                    self.gravityVy = 1
 
         if keys[K_a]:
             self.posX -= 5
@@ -48,8 +72,11 @@ class Entity:
         if self.jumping:
             self.vy += 0.25
             self.posY += self.vy
-            if self.posY > 780 and level.getSquare(self.posY) == 0:
+            self.falling = False
+            if level.getSquare(self.posY) == 0 and self.posY > 780:
+                print(self.posY)
                 self.posY = 780
+                print("THIS RAN")
                 self.jumping = False
                 self.grounded = True
                 self.vy = -8
@@ -62,10 +89,12 @@ class Entity:
                             self.posY = r2[1]-20
                             self.vy = -8
                             self.jumping = False
+                            self.falling = False
+                            self.grounded = True
                         else:
                             self.vy = 1
                         break
-
+        
         draw.circle(screen, GREEN, (player.posX, player.posY), 20)
 
 class Level():
@@ -77,11 +106,10 @@ class Level():
         if y < 0 and self.square < 2:
             self.square+=1
             player.posY = height-30
-            print("This ran 1")
         elif y > height and self.square > 0:
             self.square-=1
             player.posY = 30
-            print("This ran 2")
+            player.grounded = False
         return(self.square)
 
     def draw(self):
@@ -101,7 +129,7 @@ while running:
         if evt.type==QUIT:
             running=False
         if evt.type==KEYDOWN:
-            if evt.key == K_SPACE and not player.jumping:
+            if evt.key == K_SPACE and not player.jumping and not player.falling:
                 player.jumping = True
         if evt.type == MOUSEBUTTONDOWN:
             if evt.button == 1:
