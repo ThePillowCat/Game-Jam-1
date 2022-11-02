7309c2d8c29cf5cc9222a6d79633b404521572da
ThePillowCat <noahlevy575@gmail.com>
2022-11-01 21:51:01 -0400

    Update game.py
    Fixed some more bugs

--- game.py
+++ game.py
@@ -22,39 +22,38 @@ background = transform.scale(background, (1000,800))
 
 
 class Entity:
-    def __init__(self, x, y):
-        self.posX = x
-        self.posY = y
+    def __init__(self, x):
         self.vy = -8
         self.gravityVy = 1
         self.jumping = False
         self.falling = False
         self.grounded = True
-        self.radius = 20
-
+        self.width = 100
+        self.height = 40
+        self.posX = x
+        self.posY = height - self.height
     def movePlayer(self):
 
         keys = key.get_pressed()
 
         self.grounded = False
-
-        if not self.grounded and not self.jumping:
+        if not self.jumping:
             self.falling = True
             self.gravityVy += 0.125
             self.posY += self.gravityVy
 
-        if (level.getSquare(player.posY) == 0 and self.posY > height-player.radius):
+        if (level.getSquare(player.posY) == 0 and self.posY > height-player.height):
             self.grounded = True
-            self.posY = height-player.radius
+            self.posY = height-player.height
             self.gravityVy = 1
             self.falling = False
 
         for platform in levels[level.currentLevel][level.getSquare(player.posY)]:
-            r1 = Rect(self.posX-20, self.posY-20, 40, 42)
+            r1 = Rect(self.posX, self.posY, player.width, player.height+2)
             r2 = Rect(platform)
             if Rect.colliderect(r1, r2):
-                if (r2[1] < r1[1]+42 < r2[1]+r2[3]):
-                    self.posY = r2[1]-20
+                if (r2[1] < r1[1]+player.height < r2[1]+r2[3]):
+                    self.posY = r2[1]-player.height
                     self.grounded = True
                     self.falling = False
                     self.gravityVy = 1
@@ -62,44 +61,44 @@ class Entity:
         if keys[K_a]:
             self.posX -= 5
             for platform in levels[level.currentLevel][level.getSquare(player.posY)]:
-                r1 = Rect(self.posX-20, self.posY-20, 40, 40)
+                r1 = Rect(self.posX, self.posY, player.width, player.height)
                 r2 = Rect(platform)
-                if Rect.colliderect(r1, r2) or self.posX-20 < 0:
+                if Rect.colliderect(r1, r2) or self.posX < 0:
                     self.posX += 5
 
         if keys[K_d]:
             self.posX += 5
             for platform in levels[level.currentLevel][level.getSquare(player.posY)]:
-                r1 = Rect(self.posX-20, self.posY-20, 40, 40)
+                r1 = Rect(self.posX, self.posY, player.width, player.height)
                 r2 = Rect(platform)
-                if Rect.colliderect(r1, r2) or self.posX+20 > width:
+                if Rect.colliderect(r1, r2) or self.posX+player.width > width:
                     self.posX -= 5
-
         if self.jumping:
             self.vy += 0.25
             self.posY += self.vy
             self.falling = False
-            if level.getSquare(self.posY) == 0 and self.posY > height-player.radius:
-                self.posY = height-player.radius
+            if level.getSquare(self.posY) == 0 and self.posY > height-player.height:
+                self.posY = height-player.height
                 self.jumping = False
                 self.grounded = True
                 self.vy = -8
             else: 
                 for platform in levels[level.currentLevel][level.getSquare(player.posY)]:
-                    r1 = Rect(self.posX-20, self.posY-20, 40, 40)
+                    r1 = Rect(self.posX, self.posY, player.width, player.height+2)
                     r2 = Rect(platform)
                     if Rect.colliderect(r1, r2):
-                        if r2[1] < r1[1]+40 < r2[1]+r2[3]:
-                            self.posY = r2[1]-20
+                        if r2[1] < r1[1]+player.height+2 < r2[1]+r2[3]:
+                            self.posY = r2[1]-player.height
                             self.vy = -8
                             self.jumping = False
                             self.falling = False
                             self.grounded = True
                         else:
+                            #runs if hit bottom of rect
                             self.vy = 1
                         break
         
-        draw.circle(screen, RED, (player.posX, player.posY), player.radius)
+        draw.ellipse(screen, RED, (player.posX, player.posY, player.width, player.height))
 
 class Level():
     def __init__(self):
@@ -114,18 +113,18 @@ class Level():
             self.square-=1
             player.posY = 30
             player.grounded = False
+            print("yo")
         return(self.square)
 
     def draw(self):
         for platformIn in levels[self.currentLevel][self.getSquare(player.posY)]:
             screen.blit(platform, (platformIn[0],platformIn[1]))
 
-player = Entity(20, 760)
+player = Entity(100)
 level = Level()
 
 while running:
-    
-    # screen.fill((255, 255, 255))
+
     screen.blit(background, (0,0))
 
     mx,my=mouse.get_pos()
@@ -145,7 +144,6 @@ while running:
 
     player.movePlayer()
     level.draw()
-
     myClock.tick(60)
     display.flip()
             
